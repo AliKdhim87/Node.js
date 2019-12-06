91f0b0c9ce1407606114213f37ae1089608f7eaa
Ali Amouri Kadhim <alikadhim87nl@gmail.com>
2019-12-06 17:01:54 +0100

    I solved the issue

--- week2/HYF-NodeJs-Week-2-homework/exercise1/routes/api/blogs.js
+++ week2/HYF-NodeJs-Week-2-homework/exercise1/routes/api/blogs.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const blogs = require('../../blogs-info');
+const fs = require('fs');
+const path = require('path');
 
 // get blogs
 router.get('/', (req, res) => {
@@ -28,40 +30,49 @@ router.post('/', (req, res) => {
     if (!newBlog.title || !newBlog.content) {
         return res.status(400).json({ msg: 'Please include a title and contant' });
     }
-
     blogs.push(newBlog);
-    res.json({ blogs, msg: 'Blog added' });
 
+    fs.readFile(path.join('blogs-info.json'), (err, data) => {
+        if (err) throw err;
+        var json = JSON.parse(data);
+        json.push(newBlog);
+        fs.writeFile(path.join('blogs-info.json'), JSON.stringify(json), err => {
+            if (err) throw err;
+            console.log('The "data to append" was appended to file!');
+        });
+    })
+    res.json({ blogs, msg: 'Blog added' });
 });
 // Update a blog
 router.put('/:title', (req, res) => {
-    const found = blogs.some(item => item.title === req.params.title);
-    if (found) {
-        const updateBlog = req.body;
-        blogs.forEach(blog => {
-            if (blog.title === req.params.title) {
-                blog.title = updateBlog.title ? updateBlog.title : blog.title;
-                blog.content = updateBlog.content ? updateBlog.content : blog.content;
-                res.send(blog)
-                res.json({ msg: 'Blog updated', blog })
-            } else {
-                res.status(400)
-                res.json({ msg: `No blog with the id of ${req.params.id}` });
-            }
-        });
-    } else {
-        res.status(400);
-        res.json({ msg: `No blog with the title of ${req.params.title}` });
+
+    const title = req.params.title;
+    const blogToUpdate = blogs.find(item => item.title === title);
+
+    if (typeof blogToUpdate == 'undefined') {
+        res.status = 404;
+        res.send('No such book');
+        return;
     }
+
+    blogToUpdate.title = req.body.title;
+    blogToUpdate.content = req.body.content;
+    res.send('ok')
+
 });
 // Delete blog
 router.delete('/:title', (req, res) => {
-    const found = blogs.some(item => item.title === req.params.title);
-    if (found) {
-        res.send({ msg: 'Blog deleted', blogs: blogs.filter(item => item.title !== req.params.title) })
-    } else {
-        res.status(400).json({ msg: `No blog with the title of ${req.params.title}` });
+
+    const title = req.params.title;
+
+    const blogToDelete = blogs.find(blog => blog.title === title);
+    if (typeof blogToDelete == 'undefined') {
+        res.status = 404;
+        res.send('No such book');
+        return;
     }
+    blogs.splice(blogs.indexOf(blogToDelete), 1);
+    res.send('Blog deleted')
 
 });
 module.exports = router;
