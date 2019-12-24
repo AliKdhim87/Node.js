50d315169ec28c121362f54c3b8b12ce62194dd0
Ali Amouri Kadhim <alikadhim87nl@gmail.com>
2019-12-24 14:23:46 +0100

    fixed the issues

--- week2/HYF-NodeJs-Week-2-homework/exercise1/routes/api/blogs.js
+++ week2/HYF-NodeJs-Week-2-homework/exercise1/routes/api/blogs.js
@@ -3,76 +3,88 @@ const router = express.Router();
 const blogs = require('../../blogs-info');
 const fs = require('fs');
 const path = require('path');
+const jsonfile = require('jsonfile');
 
 // get blogs
 router.get('/', (req, res) => {
-    res.json(blogs)
+    fs.readFile(path.join('blogs-info.json'), (err, result) => {
+        if (err) throw err;
+        const data = JSON.parse(result)
+        res.json(data)
+    })
+
 });
 // if you need to get one blog 
-router.get('/:title', (req, res) => {
-    const found = blogs.some(item => item.title === req.params.title);
+router.get('/:id', (req, res) => {
+    const found = blogs.some(item => item.id === parseInt(req.params.id));
     if (found) {
-        res.send(blogs.filter(item => item.title === req.params.title))
+        res.send(blogs.filter(item => item.id === parseInt(req.params.id)))
     } else {
-        res.status(400).json({ msg: `No blog with the title of ${req.params.title}` });
+        res.status(400).json({ msg: `No blog with the title of ${parseInt(req.params.id)}` });
     }
 
 });
 // create Blog
 router.post('/', (req, res) => {
-
     const newBlog = {
+        id: req.body.id,
         title: req.body.title,
         content: req.body.content,
-
     };
-
-    if (!newBlog.title || !newBlog.content) {
+    if (!newBlog.id) {
         return res.status(400).json({ msg: 'Please include a title and contant' });
     }
     blogs.push(newBlog);
 
-    fs.readFile(path.join('blogs-info.json'), (err, data) => {
+    fs.readFile(path.join('blogs-info.json'), function(err, data) {
         if (err) throw err;
         var json = JSON.parse(data);
         json.push(newBlog);
-        fs.writeFile(path.join('blogs-info.json'), JSON.stringify(json), err => {
+        fs.writeFile(path.join('blogs-info.json'), JSON.stringify(json), function(err) {
             if (err) throw err;
             console.log('The "data to append" was appended to file!');
         });
     })
-    res.json({ blogs, msg: 'Blog added' });
+    res.json({ msg: 'Blog added' });
 });
 // Update a blog
-router.put('/:title', (req, res) => {
-
-    const title = req.params.title;
-    const blogToUpdate = blogs.find(item => item.title === title);
-
-    if (typeof blogToUpdate == 'undefined') {
-        res.status = 404;
-        res.send('No such book');
-        return;
+router.put('/:id', function(req, res) {
+    const found = blogs.some(item => item.id === parseInt(req.params.id));
+    if (!found) {
+        return res.status(400).json({ msg: `No blog with the title of ${parseInt(req.params.id)}` });
     }
+    const id = req.params.id;
+    const newContent = req.body.content;
+    const newTitle = req.body.title
 
-    blogToUpdate.title = req.body.title;
-    blogToUpdate.content = req.body.content;
-    res.send('ok')
+    // read in the JSON file
+    jsonfile.readFile(path.join('blogs-info.json'), (err, obj) => {
+        if (err) throw err;
+        // Using another variable to prevent confusion.
+        const fileObj = obj;
+        // Modify the text at the appropriate id
+        fileObj.forEach(blog => {
+            if (blog.id === parseInt(id)) {
+                blog.content = newContent;
+                blog.title = newTitle;
+                res.send("blog updated")
+            }
+        })
+        jsonfile.writeFile(path.join('blogs-info.json'), fileObj, (err) => {
+            if (err) throw err;
+        });
 
+    });
 });
 // Delete blog
-router.delete('/:title', (req, res) => {
-
-    const title = req.params.title;
-
-    const blogToDelete = blogs.find(blog => blog.title === title);
-    if (typeof blogToDelete == 'undefined') {
-        res.status = 404;
-        res.send('No such book');
-        return;
-    }
-    blogs.splice(blogs.indexOf(blogToDelete), 1);
-    res.send('Blog deleted')
-
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    const blogToDelete = blogs.some(blog => blog.id === parseInt(id));
+    if (!blogToDelete) return res.status(404).send('No such book')
+    const result = blogs.splice(blogToDelete, 1);
+    fs.writeFile('blogs-info.json', JSON.stringify(result), (err) => {
+        if (err) throw err;
+        res.json('Deleted');
+    });
 });
 module.exports = router;
